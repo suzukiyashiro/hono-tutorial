a203732097f0a06c56f4601ffdbff772eaf6ea57
suzukiyashiro <j2100095@gunma-u.ac.jp>
2024-10-12 19:05:32 +0900

    Todo作成

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,9 +1,33 @@
 import { Hono } from "hono";
+import { TodoType } from "../types";
+import { v4 as uuid } from "uuid";
 
 const todo = new Hono();
 
-todo.get("/", (c) => {
-  return c.text("hello todo");
+// todo.get("/", (c) => {
+//   return c.text("hello todo");
+// });
+
+// 空の配列
+let todos: TodoType[] = [];
+
+// Todoの作成
+todo.post("/", async (c) => {
+  const { title, desc } = await c.req.json();
+  const newTodo: TodoType = {
+    id: uuid(),
+    title,
+    desc,
+    createdAt: new Date(),
+    isCompleted: false,
+  };
+  todos.push(newTodo);
+  return c.json(newTodo, 201);
+  /* ちなみに...
+    200: all ok
+    201: all ok & something created(POST)
+    204: all ok & no return (DELETE) 
+  */
 });
 
 export { todo };
